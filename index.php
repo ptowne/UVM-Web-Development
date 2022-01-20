ec801f0123702f59c72a972e8c55d4f70b719168
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-01-20 12:53:56 -0500

    Created my sitemap

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hannah's CS008 Index</title>
+        <meta name="author" content="Hannah Sheridan">
+        <meta name="description" content="A site map to my CS008 assignments for the 2022 Spring Semester.">
+    </head>
+
+    <body>
+        <figure style="float: left; text-align: center; width: 20%;">
+
+            <img alt="Hannah Sheridan circ- 2020" 
+                 src="images/hannah-sheridan.jpg" 
+                 style="max-width: 100%;">
+            <figcaption>Heading off to my first Computer class.</figcaption>
+        </figure>
+
+        <h1>CS 008-<em>B</em> Spring 2022</h1>
+        <h2>Hannah Sheridan - Site map</h2>
+        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+
+        <h2>Lab one - Setting up your sitemap.</h2>
+        <p><a href="testing.php">Testing Page</a></p>
+
+    </body>
+</html>
+    
+
+
+
+
