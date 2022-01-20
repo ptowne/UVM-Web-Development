8f69c9aaa629a05fa2d40184dd9b9a89a21422a1
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-01-20 13:11:33 -0500

    created my test page

---
+++ testing.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+    <meta charset = "utf-8">
+    <meta name = "author" content = "Hannah Sheridan">
+    <meta name = "description" content = "my testing page">
+    <title>Testing Page</title>
+    <body>
+        <h1>Hannah's Testing Page</h1>
+        <p>I'm super excited for the winter, the snow is beautiful and I love to ski!</p>
+    </body>
+</html>
