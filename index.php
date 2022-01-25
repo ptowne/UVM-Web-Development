ea8e17e63dcc712f5779100988c7407e0c498263
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-01-25 17:37:40 -0500

    added lab two to sitemap
    also added a public files, supporting files, and notes to grader section to both labs

--- index.php
+++ index.php
@@ -19,10 +19,51 @@
         <h1>CS 008-<em>B</em> Spring 2022</h1>
         <h2>Hannah Sheridan - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+<!-- Section: Lab Two -->
+        <section class = "grid-layout">
+            <h2 class = "header">Lab Two - Making the World a Better Place</h2>
+        <!-- Public files = lab2/index.php -->
+            <section class = "public-files">
+                <h3>Public Files</h3>
+                <p><a href = "lab2/index.php">index.php</a></p>
+            </section>
 
-        <h2>Lab one - Setting up your sitemap.</h2>
-        <p><a href="testing.php">Testing Page</a></p>
+        <!-- Supporting files = lab2/css/custom.css -->
+            <section class = "supporting-files">
+                <h3>Supporting Files</h3>
+                <p><a href = "lab2/css/custom.css">custom.css</a></p>
+                <p></p>
+            </section>
 
+        <!-- Notes to grader -->
+            <section class = "grader-notes">
+                <h3>Notes to Grader</h3>
+                <p></p>
+            </section>
+        </section>
+    <!-- Section: Lab One -->
+        <section class = "grid-layout">
+            <h2 class = "header">Lab One - Setting up your Sitemap</h2>
+        
+        <!-- Public files = testing.php -->
+            <section class = "public-files">
+                <h3>Public Files</h3>
+                <p><a href = "testing.php">Testing Page</a></p>
+            </section>
+        
+        <!-- Supporting files -->
+            <section class = "supporting-files">
+                <h3>Supporting Files</h3>
+                <p><a href = "lab2/css/custom.css">custom.css</a></p>
+                <p></p>
+            </section>
+
+        <!-- Notes to grader -->
+            <section class = "grader-notes">
+                <h3>Notes to Grader</h3>
+                <p></p>
+            </section>
+        </section>
     </body>
 </html>
     
