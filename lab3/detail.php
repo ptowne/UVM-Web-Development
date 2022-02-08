efdef6e0c3377eb6423e32be787565ee57afd548
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-02-08 17:11:40 -0500

    added page for detailed timeline

---
+++ lab3/detail.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <!-- meta tags and title -->
+    <meta charset="utf-8">
+    <title>Plant More Trees</title>
+    <meta name="author" content="Hannah Sheridan">
+    <!-- description: about trees, deforestation, and how planting can save the environment -->
+    <meta name="description" content="Around 80,000 acres of tropical rainforest are cut down every day. Deforestation has ridden our planet and is
+        killing one of the earth's most helpful and beautiful plants: trees. It is uncertain whether this deforestation will ever end, but we can ease its
+        damaging effects by planting more trees.">
+    <link rel="stylesheet" href="css/cusstom.css?version=<?php print time(); ?>" type="text/css">
+</head>
+
+<body class = "if-needed">
+    <header>
+        <!-- main header: plant more trees -->
+        <h1>Plant More Trees</h1>
+        <h2>Deforestation's damaging effects and how we can stop it</h2>
+    </header>
+    <figure>
+        <img src="images/planting-tree.jpg" alt="Planting a Tree" width="300">
+        <figcaption>Plant a Tree [photo: Greener Land]</figcaption>
+    </figure>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="detail.php">Detail</a>
+        <a href="form.php">Form</a>
+    </nav>
+    <main>
+        <section>
+            <h1>A More Detailed Timeline of Deforestation</h1>
+            <section>
+                <h3>1700-1850</h3>
+                <p>The Industrial Revolution (<em>1760-1840</em>) led to a rapid increase in deforestation all over the world. From 1700 to 1850, more than 46 million
+            acres of trees (half the size of Germany) were destroyed.</p>
+            </section>
+            <section>
+                <h3>1850-1920</h3>
+                <p>Rapid population growth and the invention of the rubber tire in 1914 led to a new wave of deforestation, causing tree loss to rise 50%.
+                    Trees lost per decade increased to around 74 million acres.</p>
+            </section>
+            <section>
+                <h3>1920-1930</h3>
+                <p>The Great Depression caused many farmers to leave their land behind, allowing forests some time to regrow.</p>
+            </section>
+            <section>
+                <h3>1940-1980</h3>
+                <p>The construction of the Belem-Brasilia Highway (1950) and the Transamazon Highway (1970) in Brazil caused deforestation rates to increase futher.
+                    After these projects, tree loss rose to more than 296 million acres per decade.</p>
+            </section>
+            <section>
+                <h3>1980</h3>
+                <p>Global deforestation reaches its peak: up to 370 million acres of trees lost per decade. Government investments
+                    in infrastructure and agriculture encouraged forest destruction.</p>
+            </section>
+            <section>
+                <h3>1990-2000</h3>
+                <p>Eventually, countries reach a <strong>forest transition point</strong> where more trees are regrown than lost. Around
+                the end of 1990, forests reached their transition points, causing tree loss to decrease to 128 million in 2000.</p>
+            </section>
+            <section>
+                <h3>Today and Onward</h3>
+                <p>In the past decade, we lost around 116 million acres of trees, and around 14 million acres of forest were able to regrow. 
+                    In order to continue this positive turn for the better, we must keep in mind the impact we have on our earth. If we 
+                    continue deforestation as we have been, the beautiful Amazon rainforest could be destroyed beyond repair.</p>
+                <p>If you are interested in stopping the damaging effects of deforestation, here are a few organizations that work
+                    to better the state of our rainforests:
+                    <ul>
+                        <li><a href = "https://amazonwatch.org/">Amazon Watch</a></li>
+                        <li><a href = "https://borneoproject.org/">Boreneo Project</a></li>
+                        <li><a href = "https://survivalinternational.org/">Survival International</a></li>
+                        <li><a href = "https://www.gcearth.org/">Go Conscious Earth</a></li>
+                        <li>Source: <a href = "https://www.worldrainforest.org/rainforest-organizations.html">worldrainforest.org</a></li>
+                    </ul>
+            </section>
+    </main>
+
+</body>
+
+</html>
