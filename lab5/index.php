15ef9e60d348faf4ba9356cfa94687e2d3520221
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-02-22 14:33:55 -0500

    added index.php to lab5

--- lab5/index.php
+++ lab5/index.php
@@ -1,1 +1,186 @@
+<!DOCTYPE HTML>
+<html lang="en">
 
+<head>
+    <!-- meta tags and title -->
+    <meta charset="utf-8">
+    <title>Plant More Trees</title>
+    <meta name="author" content="Hannah Sheridan">
+    <!-- description: about trees, deforestation, and how planting can save the environment -->
+    <meta name="description" content="Around 80,000 acres of tropical rainforest are cut down every day. Deforestation has ridden our planet and is
+        killing one of the earth's most helpful and beautiful plants: trees. It is uncertain whether this deforestation will ever end, but we can ease its
+        damaging effects by planting more trees.">
+    <link rel="stylesheet" href="css/custom.css?version=<?php print time();?>" type="text/css">
+</head>
+
+<body class="if-needed">
+    <header>
+        <!-- main header: plant more trees -->
+        <h1>Plant More Trees</h1>
+        <h2>Deforestation's damaging effects and how we can stop it</h2>
+    </header>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="detail.php">Timeline</a>
+        <a href="form.php">Form</a>
+    </nav>
+    <main>
+        <figure class = "deforestation">
+            <img src="images/deforestation.jpg" alt="Deforestation" width="300" class = "deforestation">
+            <figcaption>Slash and Burn Deforestation [photo: Getty Images] <cite>Getty Images</cite></figcaption>
+        </figure>
+        <section class="what-is">
+            <!-- Header to talk about the history of the forests -->
+            <h2>What is Deforestation?</h2>
+            <p>Forests cover 30% of our earth's land. <strong>Deforestation</strong> is the purposeful destruction of
+                these
+                forests for human use. This
+                can be done by chopping or burning down large plots of land. Deforestation is often used to make space
+                for
+                animals, agriculture, or new construction. People also deforest to gather wood for fuel or building
+                materials.
+                As a result, we lose around 80,000 acres of tropical rainforest every single day (<em>approximately 27
+                    football fields per minute</em>).</p>
+            <!-- table with deforestation timeline -->
+            <table>
+                <caption><strong>A Timeline of Deforestation</strong></caption>
+                <tr>
+                    <th>Year</th>
+                    <th>Cause</th>
+                    <th>Trees Lost per Decade</th>
+                </tr>
+                <tr>
+                    <td>1700s-1800s</td>
+                    <td>Industrial Revolution</td>
+                    <td>~47 million acres</td>
+                </tr>
+                <tr>
+                    <td>1850-1920</td>
+                    <td>Invention of the rubber tire</td>
+                    <td>~74 million acres</td>
+                </tr>
+
+                <tr>
+                    <td>1950-1980</td>
+                    <td>Construction of Belem-Brasilia Highway and Transamazon Highway</td>
+                    <td>~286 million acres</td>
+                </tr>
+                <tr>
+                    <td>1990-2020</td>
+                    <td>Forests reach their <a href="https://en.wikipedia.org/wiki/Forest_transition"
+                            target="_blank"><strong>forest transition point</strong></a></td>
+                    <td>~192 million acres (late 1900s) to ~116 million acres (2000s)</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Sources: <cite><a href="https://ourworldindata.org/global-deforestation-peak"
+                                target="_blank">
+                                https://ourworldindata.org/global-deforestation-peak</a></cite>
+                        <cite><a href="https://www.timetoast.com/timelines/deforestation-16f20047-819b-49db-a0f4-d258bbacb3c3"
+                                target="_blank">https://www.timetoast.com/timelines/deforestation-16f20047-819b-49db-a0f4-d258bbacb3c3</a></cite>
+                    </td>
+                </tr>
+            </table>
+        </section>
+
+        <!-- Header about the effects of deforestation -->
+        <section class="effects">
+            <h2>The Damaging Effects of Deforestation</h2>
+            <!-- P1: about slash and burn method -->
+            <p>As we know by now, deforestation is occuring every day on our earth as a result of construction,
+                agriculture,
+                and other human activity.
+                One type of deforestation, used for agriculture, is called the slash and burn method, in which farmers
+                burn
+                plots of forests to clear farmland and fertilize the ground with the ash from the burnt trees. This
+                soil,
+                however, becomes
+                unusable after a few years, so farmers must move to another location and repeat the method.</p>
+            <!--P2: about the effects of slash and burn method -->
+            <p>Trees hold carbon dioxide in their wood. When they are burned, this carbon dioxide goes up into the
+                atmosphere, resulting in increased levels
+                of global warming. With the slash and burn method, large plots of land are burned, used for agriculture,
+                then abandoned. The temporary positive effects
+                of the slash and burn method worsens deforestation because farmers are forced to leave the land and find
+                another plot to clear out every few years. This justs
+                adds to the accumulation of carbon dioxide in our atmosphere, accelerating global warming to concerning
+                levels.</p>
+
+            <!--P3: other negative effects of deforestation -->
+            <p>In addition to the unnerving amounts of carbon dioxide in the atmosphere, deforestation also contributes
+                soil
+                erosion and flooding. Trees provide
+                nutrients to soil which helps support the growth of other plants in the forest. When farmers can no
+                longer
+                use their land, they leave behind barren
+                and infertile soil, which is more susceptable to flooding and cannot support forest life. Deforestation
+                also
+                destroys animal habitats, as many animals
+                take shelter under trees and canopies. Deforestation also causes drastic temperature changes, which
+                makes
+                animal's habitats more inhospitable.</p>
+        </section>
+        <!-- Header and list about how trees can help the environment -->
+        <figure class = "forest">
+            <img src="images/forest.jpg" alt="Forest of Trees" width="300" class = "forest">
+            <figcaption>Lush Forest [photo: infoguides] <cite>infoguides</cite></figcaption>
+        </figure>
+        <section class="why-trees">
+            <h2>How Trees Help our Earth</h2>
+            <ul>
+                <li><strong>Trees provide oxygen</strong>
+                    <ul>
+                        <li>Trees take in carbon dioxide to create oxygen for us to breathe, reducing CO2 levels in the
+                            process. While the ocean provides most of the earth's oxygen, rainforests account for around
+                            28%
+                            of the earth's oxygen.</li>
+                    </ul>
+                </li>
+                <li><strong>Trees provide habitats for animals</strong>
+                    <ul>
+                        <li>Trees give a variety of animals and insects shelter and food. Trees improve the ecostystem;
+                            wildlife often choose to be in areas with a good amount of trees.</li>
+                    </ul>
+                </li>
+                <li><strong>Trees reduce erosion and flooding</strong>
+                    <ul>
+                        <li>A Tree's leaves catches rain as it falls, lessening the amount of water that reaches the
+                            ground
+                            and reducing flooding. Trees also have large roots that absorb water and hold soil in place,
+                            reducing erosion.</li>
+                    </ul>
+                </li>
+                <li><strong>Trees decrease air pollutants</strong>
+                    <ul>
+                        <li>Trees trap air pollutants in their leaves, filtering our air and making it easier for us to
+                            breathe.</li>
+                    </ul>
+                </li>
+                <li><strong>Trees lower temperatures</strong>
+                    <ul>
+                        <li>When trees evaporate water, it cools the area around them. This tempers the climate and
+                            reduces
+                            humidity.</li>
+                    </ul>
+                </li>
+                <li>Sources: <cite><a href="http://www.nitrofill.com/TreeHelp.aspx"
+                            target="_blank">http://www.nitrofill.com/TreeHelp.aspx</a></cite>
+                    <cite><a href="https://www.nwf.org/Trees-for-Wildlife/About/Trees-Make-a-Difference"
+                            target="_blank">https://www.nwf.org/Trees-for-Wildlife/About/Trees-Make-a-Difference</a></cite>
+                </li>
+            </ul>
+        </section>
+        <!-- Section for other sources used -->
+        <p>Other Sources Used:
+            <cite><a href="https://www.nationalgeographic.org/encyclopedia/deforestation/#:~:text=Deforestation%20is%20the%20purposeful%20clearing%20of%20forested%20land.&text=About%202%2C000%20years%20ago%2C%2080,1870s%20for%20timber%20and%20agriculture"
+                    target="_blank">National Geographic</a></cite>
+            <cite><a
+                    href="https://www.scientificamerican.com/article/earth-talks-daily-destruction/#:~:text=Pinning%20down%20exact%20numbers%20is,day%20on%20top%20of%20that.">Scientific
+                    American</a></cite>
+        </p>
+    </main>
+    <footer>
+        <p><a href = "../index.php">Site map (Main index)</a></p>
+    </footer>
+</body>
+
+</html>
