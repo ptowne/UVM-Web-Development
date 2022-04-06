954fb77a3f2eb6a8f859514ee6fb4390e7500aae
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-04-05 23:28:26 -0400

    Lab 8: display data

--- lab8/index.php
+++ lab8/index.php
@@ -1,1 +1,159 @@
+<?php
+include 'top.php';
+?>
+    <main class = "main">
+        <figure class = "deforestation">
+            <img src="images/deforestation.jpg" alt="Deforestation" width="300" class = "deforestation-photo">
+            <figcaption>Slash and Burn Deforestation [photo: Getty Images] <cite>Getty Images</cite></figcaption>
+        </figure>
+        <section class="what-is">
+            <!-- Header to talk about the history of the forests -->
+            <h2>What is Deforestation?</h2>
+            <p>Forests cover 30% of our earth's land. <strong>Deforestation</strong> is the purposeful destruction of
+                these
+                forests for human use. This
+                can be done by chopping or burning down large plots of land. Deforestation is often used to make space
+                for
+                animals, agriculture, or new construction. People also deforest to gather wood for fuel or building
+                materials.
+                As a result, we lose around 80,000 acres of tropical rainforest every single day (<em>approximately 27
+                    football fields per minute</em>).</p>
+        </section>
+            <!-- table with deforestation timeline -->
+        <section class = "timeline-table">
+            <h2>Deforestation Over the Years</h2>
+            <table class>
+                <caption><strong>A Timeline of Deforestation</strong></caption>
+                <tr>
+                    <th>Year</th>
+                    <th>Cause</th>
+                    <th>Trees Lost per Decade</th>
+                </tr>
+<?php
+$sql = 'SELECT fldYear, fldCause, fldEffect FROM tblTimeline';
 
+$statement = $pdo->prepare($sql);
+$statement->execute();
+
+$records = $statement->fetchAll();
+
+foreach ($records as $record) {
+    print '<tr>';
+    print '<td>' . $record['fldYear'] . '</td>';
+    print '<td>' . $record['fldCause'] . '</td>';
+    print '<td>' . $record['fldEffect'] . '</td>';
+    print '</tr>' . PHP_EOL;
+}
+?>
+                <tr>
+                    <td colspan="3">Sources: <cite><a href="https://ourworldindata.org/global-deforestation-peak"
+                                target="_blank">
+                                https://ourworldindata.org/global-deforestation-peak</a></cite>
+                        <cite><a href="https://www.timetoast.com/timelines/deforestation-16f20047-819b-49db-a0f4-d258bbacb3c3"
+                                target="_blank">https://www.timetoast.com/timelines/deforestation-16f20047-819b-49db-a0f4-d258bbacb3c3</a></cite>
+                    </td>
+                </tr>
+            </table>
+        </section>
+
+        <!-- Header about the effects of deforestation -->
+        <section class="effects">
+            <h2>The Damaging Effects of Deforestation</h2>
+        </section>
+            <!-- P1: about slash and burn method -->
+                <figure class = "slash">
+                    <img src = "images/slashandburn.jpg" alt = "Slash and Burn Deforestation" class = "slash-photo">
+                    <figcaption>Slash and Burn Agriculture [photo: DW] <cite>DW</cite></figcaption>
+                </figure>
+            <section class = "slashandburn">
+                <h4>The Slash and Burn Method</h4>
+                <p>As we know by now, deforestation is occuring every day on our earth as a result of construction,
+                agriculture,
+                and other human activity.
+                One type of deforestation, used for agriculture, is called the slash and burn method, in which farmers
+                burn
+                plots of forests to clear farmland and fertilize the ground with the ash from the burnt trees. This
+                soil,
+                however, becomes
+                unusable after a few years, so farmers must move to another location and repeat the method.</p>
+                </section>  
+                <!--P2: about the effects of slash and burn method -->
+            <section class = "slashandburn-effects">
+                <h4>Effects of the Slash and Burn Method</h4>
+                <p>Trees hold carbon dioxide in their wood. When they are burned, this carbon dioxide goes up into the
+                atmosphere, resulting in increased levels
+                of global warming. With the slash and burn method, large plots of land are burned, used for agriculture,
+                then abandoned. The temporary positive effects
+                of the slash and burn method worsens deforestation because farmers are forced to leave the land and find
+                another plot to clear out every few years. This justs
+                adds to the accumulation of carbon dioxide in our atmosphere, accelerating global warming to concerning
+                levels.</p>
+            <!--P3: other negative effects of deforestation -->
+            </section>
+            <section class = "other-effects">
+                <h4>Other Negative Effects of Deforestation</h4>
+                <p>In addition to the unnerving amounts of carbon dioxide in the atmosphere, deforestation also contributes
+                soil
+                erosion and flooding. Trees provide
+                nutrients to soil which helps support the growth of other plants in the forest. When farmers can no
+                longer
+                use their land, they leave behind barren
+                and infertile soil, which is more susceptable to flooding and cannot support forest life. Deforestation
+                also
+                destroys animal habitats, as many animals
+                take shelter under trees and canopies. Deforestation also causes drastic temperature changes, which
+                makes
+                animal's habitats more inhospitable.</p>
+            </section>
+        <!-- Header and list about how trees can help the environment -->
+        <section class="why-trees">
+            <h2>How Trees Help our Earth</h2>
+            <ul>
+                <li><strong>Trees provide oxygen</strong>
+                    <!--<ul>
+                        <li>Trees take in carbon dioxide to create oxygen for us to breathe, reducing CO2 levels in the
+                            process. While the ocean provides most of the earth's oxygen, rainforests account for around
+                            28%
+                            of the earth's oxygen.</li>
+                    </ul>-->
+                </li>
+                <li><strong>Trees provide habitats for animals</strong>
+                    <!--<ul>
+                        <li>Trees give a variety of animals and insects shelter and food. Trees improve the ecostystem;
+                            wildlife often choose to be in areas with a good amount of trees.</li>
+                    </ul>-->
+                </li>
+                <li><strong>Trees reduce erosion and flooding</strong>
+                    <!--<ul>
+                        <li>A Tree's leaves catches rain as it falls, lessening the amount of water that reaches the
+                            ground
+                            and reducing flooding. Trees also have large roots that absorb water and hold soil in place,
+                            reducing erosion.</li>
+                    </ul>-->
+                </li>
+                <li><strong>Trees decrease air pollutants</strong>
+                    <!--<ul>
+                        <li>Trees trap air pollutants in their leaves, filtering our air and making it easier for us to
+                            breathe.</li>
+                    </ul>-->
+                </li>
+                <li><strong>Trees lower temperatures</strong>
+                    <!--<ul>
+                        <li>When trees evaporate water, it cools the area around them. This tempers the climate and
+                            reduces
+                            humidity.</li>
+                    </ul>-->
+                </li>
+                <li>Sources: <cite><a href="http://www.nitrofill.com/TreeHelp.aspx"
+                            target="_blank">http://www.nitrofill.com/TreeHelp.aspx</a></cite>
+                    <cite><a href="https://www.nwf.org/Trees-for-Wildlife/About/Trees-Make-a-Difference"
+                            target="_blank">https://www.nwf.org/Trees-for-Wildlife/About/Trees-Make-a-Difference</a></cite>
+                </li>
+            </ul>
+        </section>
+    </main>
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
