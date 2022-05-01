f4a9344a987ad317a3a7542286ed970c40f1c2f1
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2022-04-30 20:58:06 -0400

    Final Project: worked on the detail page

---
+++ final/about.php
@@ -0,0 +1,74 @@
+<?php
+include 'top.php';
+?>
+    <main class = "main">
+        <section class = "aboutus">
+            <h2>About Us</h2>
+            <h3>What is The Long Trail?</h3>
+                <p>The Long Trail follows the ridge of the Green Mountains from the Massachusetts-Vermont Border to the Canadian Border. On the beautiful trail, you will encounter 
+                    ponds, streams, forests, wildlife, and other magnificent views. It is a very popular hiker's destination; each year, hundreds of adventurers come for backpacking, camping, 
+                    hiking, and even horsebackriding! Hiking the Long Trail, you are sure to encounter other passionate hikers while still being able to enjoy the solitude of nature. 
+                    The trail has been completed in just 5 days, but typically takes a few weeks to complete. At The Long Trail Hiking Camp, we offer hiking and backpacking expeditions that 
+                    explore different parts of The Long Trail, as well as a backpacking trip that lets campers complete the entire trail!
+                </p>
+                <form action = "images/LongTrailMap.jpg" target = "_blank">
+                    <input type = "submit" value = "See Map" name = "btnLongTrailMap">
+                </form>
+            <h3>Our Mission</h3>
+                <p>Our mission at The Long Trail Hiking Camp is to teach people of all ages about the wonders of the backcountry of Vermont. At our camp, we take people on expeditions around
+                    the Long Trail, either hiking or backpacking, at various times throughout the summer. These trips allow our campers to meet new friends, build communities, see magnificent views, 
+                    eat amazing food, and learn about and explore the beautiful Vermont outdoors! For children, we have a fun day camp that allows campers to explore the outdoors and promotes healthy relationships with
+                    nature and prepares children to become hiking masters! We want to unite everyone under Mother Nature and create memories that will last a lifetime.
+                </p>
+            <h3>Why Explore the Magnifient Long Trail?</h3>
+                <p>
+                    TEXT TEXT TEXT
+                </p>
+        </section>
+        <section class = "tripLeaders">
+            <h2>Trip Leaders</h2>
+            <p>Our trip leaders are passionate about nature and ready to share their knowledge!</p>
+            <section class = "leader1">
+                <figure class = "leader1Figure">
+                    <img src = "images/leader.jpg" alt = "Trip Leader" width = "200" class = "leader1Photo">
+                </figure>
+                <h3>Leader One</h3>
+                <P>
+                    about leader one
+                </p>
+            </section>
+            <section class = "leader2">
+                <figure class = "leader2Figure">
+                    <img src = "images/leader.jpg" alt = "Trip Leader" width = "200" class = "leader2Photo">
+                </figure>
+                <h3>Leader Two</h3>
+                <p>
+                    about leader two
+                </p>
+            </section>
+            <section class = "leader3">
+                <figure class = "leader3Figure">
+                    <img src = "images/leader.jpg" alt = "Trip Leader" width = "200" class = "leader3Photo">
+                </figure>
+                <h3>Leader Three</h3>
+                <p>
+                    about leader three
+                </p>
+            </section>
+            <section class = "leader4">
+                <figure class = "leader4Figure">
+                    <img src = "images/leader.jpg" alt = "Trip Leader" width = "200" class = "leader4Photo">
+                </figure>
+                <h3>Leader Four</h3>
+                <p>
+                    about leader four
+                </p>
+            </section>
+        </section>
+
+    </main>
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
